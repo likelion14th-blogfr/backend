156e0095e8ed3c88a42562a615ec6156210a5fcf
Sua Jun <mjun3712@gmail.com>
2026-03-30 19:46:17 +0900

    feat: repository'

---
+++ src/main/java/likelion14th/blogfr/like/ArticleLikeRepository.java
@@ -0,0 +1,6 @@
+package likelion14th.blogfr.like;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+}
