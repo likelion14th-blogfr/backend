c4e9a19458392f82ed0177973c699a332e3ee965
Sua Jun <mjun3712@gmail.com>
2026-03-30 20:49:02 +0900

    feat: datasql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into users (login_id, password) values ('user1', '1234');
-insert into users (login_id, password) values ('user2', '1234');
+insert into users (login_id, nickname, password) values ('user1', '전수아', '1234');
+insert into users (login_id, nickname, password) values ('user2', '유지오', '1234');
