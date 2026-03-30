1451870da9b9d2d355f1dc81a065db8d254534e7
Sua Jun <mjun3712@gmail.com>
2026-03-30 19:06:20 +0900

    first commit

---
+++ src/main/java/likelion14th/blogfr/BlogfrApplication.java
@@ -0,0 +1,13 @@
+package likelion14th.blogfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogfrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogfrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion14th/blogfr/BlogfrApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion14th.blogfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogfrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
